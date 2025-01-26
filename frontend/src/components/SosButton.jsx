b5a80b0c2479a817da9564a81e17d16e46f23196
Akshat Singh Thakur <developerakshatsingh@gmail.com>
2025-01-26 15:00:33 +0530

    sos button

--- frontend/src/components/SosButton.jsx
+++ frontend/src/components/SosButton.jsx
@@ -9,7 +9,7 @@ const SosButton = () => {
     }
 
   return (
-    <div className='h-100 w-100 bg-blue-600 rounded-full absolute bottom-0 m-6 cursor-pointer' onClick={handleAmbulanceMessage} >
+    <div className='h-100 w-100 bg-blue-600 rounded-full absolute bottom-0 m-6 cursor-pointer ' onClick={handleAmbulanceMessage} >
         <p className='text-white p-6 hover:p-7 transition-all' >SOS</p>
     </div>
   )
