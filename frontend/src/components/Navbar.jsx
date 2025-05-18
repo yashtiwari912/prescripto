2d1ed457358308b9ed0f680bb39f725caa0c46a0
yashtiwari912 <yashtiwari92003@gmail.com>
2025-05-18 13:07:33 +0530

    Testing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/hospital-list' >
-          <li className='py-1'>Hospitals</li>
+          <li className='py-1'>HOSPITALS</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/about' >
